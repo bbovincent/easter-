b8ca86f1e8f1bf2dd58df5c11efbef9b8618c535
bbovincent <liangjyong5669@gmail.com>
2025-04-17 15:39:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function page1(){
+	window.location.href="page1.html"
+}
+function page2(){
+	window.location.href="page2.html"
+}
+function page3(){
+	window.location.href="page2.html"
+}
+function page4(){
+	window.location.href="page4.html"
+}
